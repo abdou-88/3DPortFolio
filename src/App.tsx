fa040173b583eb56babc7c9e4fc19289a2441026
Abdellah Yousfi <abdellah.y.ouahabi@gmail.com>
2023-05-02 18:10:48 +0200

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,70 @@
 
 
-import './App.css'
+import './App.css';
 
-function App() {
- 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+
+type Props = {};
+
+const Home: React.FC = () => {
+  return <h1>Home Page</h1>;
+};
+
+const Skills: React.FC = () => {
+  return <h1>Skills Page</h1>;
+};
+const Experience: React.FC = () => {
+  return <h1>Experience Page</h1>;
+};
+const Education: React.FC = () => {
+  return <h1>Education Page</h1>;
+};
+
+const NotFound: React.FC = () => {
+  return <h1>Makayen walo</h1>;
+};
+
+
+const App: React.FC<Props> = () => {
+
   return (
-    <>
-      
-      <h1>Initial project with vite</h1>
-      
-    </>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/Experience">Experience</Link>
+            </li>
+            <li>
+              <Link to="/Education">Education</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>      
+
+          <Route path="/" element={<Home/>} />
+          <Route path="/*"element={<NotFound/>} />
+          <Route path="/Skills" element={<Skills/>} />
+          <Route path="/Experience" element={<Experience/>} />
+          <Route path="/Education" element={<Education/>} />
+
+          
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  );
+};
+
+
+
 
 export default App
