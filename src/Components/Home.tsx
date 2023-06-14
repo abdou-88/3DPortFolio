133e878dde4ad9da1a1d996d19408e50cc2ccbcb
Abdellah Yousfi <abdellah.y.ouahabi@gmail.com>
2023-06-14 16:58:47 +0200

    color while scrolling

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -29,7 +29,7 @@ const Home: React.FC<Props> = () => {
         fov: 55
       }}
     >
-      <color attach="background" args={["#101010"]} />
+      <color attach="background" args={["hsl(0%, 100% ,0%)"]} />
       <fog attach="fog" args={["#101010", 15, 30]} />
 
           
@@ -54,60 +54,60 @@ const Home: React.FC<Props> = () => {
             
               <mesh scale={4} position={[4, 0, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
                 <ringGeometry args={[0.9, 1, 3, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
               
               <mesh scale={4} position={[-3, 0, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
                 <ringGeometry args={[0.9, 1, 5, 2]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
 
               <mesh scale={4} position={[-1.5, 0, 7]} rotation={[-Math.PI / 2, 0, Math.PI /2.5]}>
                 <ringGeometry args={[0.9, 1, 4, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
              
               <mesh scale={4} position={[6, 0, 5.5]} rotation={[-Math.PI / 2, 0, Math.PI / 5]}>
                 <ringGeometry args={[0.9, 1, 6, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
 
 
 
               <mesh scale={4} position={[9, 0, -5]} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
                 <ringGeometry args={[0.9, 1, 5, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
               
               <mesh scale={4} position={[11, 0, 10]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
                 <ringGeometry args={[0.9, 1, 3, 2]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
 
               <mesh scale={4} position={[-11, 0, 5]} rotation={[-Math.PI / 2, 0, Math.PI /2.5]}>
                 <ringGeometry args={[0.9, 1, 6, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
              
               <mesh scale={4} position={[3, 0, 14]} rotation={[-Math.PI / 2, 0, Math.PI / 5]}>
                 <ringGeometry args={[0.9, 1, 4, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
 
 
               <mesh scale={4} position={[15, 0, 2]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
                 <ringGeometry args={[0.9, 1, 4, 2]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
 
               <mesh scale={4} position={[-7, 0, 14]} rotation={[-Math.PI / 2, 0, Math.PI /2.5]}>
                 <ringGeometry args={[0.9, 1, 3, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
              
               <mesh scale={4} position={[3, 0, 14]} rotation={[-Math.PI / 2, 0, Math.PI / 5]}>
                 <ringGeometry args={[0.9, 1, 4, 1]} />
-                <meshStandardMaterial color="white" roughness={0.75} />
+                <meshStandardMaterial color="black" roughness={0.75} />
               </mesh>
 
               <Desk />
